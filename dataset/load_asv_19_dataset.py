b8825b452d75ca4ed13fc244f3d0fe03d6d853d0
IgnazioGul <ignazio.gulino@aitho.it>
2024-02-26 18:32:21 +0100

    refactor: minor ref

--- dataset/load_asv_19_dataset.py
+++ dataset/load_asv_19_dataset.py
@@ -1,14 +1,13 @@
 import os
 
-import cv2
 import librosa
 import numpy as np
-from audiomentations import PitchShift, TimeStretch, AddGaussianNoise
 from torch.utils.data import Dataset, DataLoader
 from typing_extensions import Literal
 
-from utils.audio_utils import AudioUtils
-from utils.timi_tts_constants import AUDIO_KEY, CLASS_KEY
+from dataset.loader_utils import preprocess_item
+from utils.augmentations import apply_time_shift, asv19_attack_class_based
+from utils.timi_tts_constants import AUDIO_KEY, CLASS_KEY, ORIGINAL_SPEC_KEY
 
 
 class LoadAsvSpoof19(Dataset):
@@ -28,23 +27,36 @@ class LoadAsvSpoof19(Dataset):
         "test": os.path.join(labels_folder, "ASVspoof2019.LA.cm.eval.trl.txt")
     }
 
-    def __init__(self, base_path: str, partition: Literal["training", "validation", "test"],
-                 model_name: Literal["resnet18", "resnet34", "resnet50", "attVgg16", "passt2"],
-                 is_validation_enabled: bool = True,
-                 mode: Literal["normal", "reduced"] = "normal",
-                 is_augment_enabled: bool = False,
-                 transform: bool = False):
+    silenced_partitions_labels = {
+        "training": "unsupported",
+        "validation": os.path.join(labels_folder, "ASVspoof2019.LA.cm.dev.filtered.trl.txt"),
+        "test": "unsupported"
+    }
 
+    def __init__(self, base_path: str, partition: Literal["training", "validation", "test"],
+                 model_name: Literal["resnet18", "resnet34", "resnet50", "attVgg16", "passt"],
+                 is_augment_enabled: bool = False,
+                 transform: bool = False,
+                 should_return_waveform: bool = False,
+                 extract_manual_spec: bool = False,
+                 is_asv19_silence_version: bool = False):
+
+        self.is_asv19_silence_version = is_asv19_silence_version
+        self.extract_manual_spec = extract_manual_spec
+        self.should_return_waveform = should_return_waveform
         self.base_path = base_path
         self.model_name = model_name
         self.is_augment_enabled = is_augment_enabled
-        # self.mode = mode
         self.transform = transform
         self.clean_train_audios_df = None
         self.partition = partition
-
+        partition_labels_path = self.silenced_partitions_labels[self.partition] if self.is_asv19_silence_version else \
+            self.partitions_labels[self.partition]
         self.audios = np.loadtxt(os.path.join(
-            self.base_path, self.partitions_labels[self.partition]), dtype=str, delimiter=' ')
+            self.base_path, partition_labels_path), dtype=str, delimiter=' ')
+        # RUN TO GENERATE FILTERED LABEL FILE
+        # if self.is_asv19_silence_version:
+        #     self._filter_missing_audios()
 
         np.random.seed(123)
         np.random.shuffle(self.audios)
@@ -61,25 +73,58 @@ class LoadAsvSpoof19(Dataset):
         :return:
         """
         _, audio_filename, _, system_id, key = self.audios[index]
-        [audio, sr] = librosa.load(
-            os.path.join(self.base_path, self.partitions_path[self.partition], audio_filename + ".flac"),
-            sr=None)
-        spec_img, spec = self._preprocess_item(audio, sr)
+        full_file_path = os.path.join(self.base_path, self.partitions_path[self.partition],
+                                      audio_filename + ".wav" if self.is_asv19_silence_version else audio_filename + ".flac")
+        [audio, sr] = librosa.load(full_file_path, sr=None)
+        spec_img, spec = self.preprocess_item(audio, sr)
         audio_class = self.classes_labels[system_id]
-        return {AUDIO_KEY: spec_img, CLASS_KEY: audio_class, "original": spec}
+        if self.is_augment_enabled and self.partition == "training":
+            if np.random.rand() <= 0.7:
+                aug_audio = asv19_attack_class_based(audio, audio_class, (2, 1), 40, 0.2, sr=sr)
+            else:
+                ts_sec = np.random.uniform(1, 3)
+                aug_audio = apply_time_shift(audio, sr, ts_sec)
+            aug_spec_img, aug_spec = self.preprocess_item(audio, sr)
+            return [{AUDIO_KEY: aug_spec_img, CLASS_KEY: audio_class, ORIGINAL_SPEC_KEY: aug_spec},
+                    {AUDIO_KEY: spec_img, CLASS_KEY: audio_class, ORIGINAL_SPEC_KEY: spec}]
+        return {AUDIO_KEY: spec_img, CLASS_KEY: audio_class, ORIGINAL_SPEC_KEY: spec}
+
     def __len__(self):
         return len(self.audios)
 
-    def _preprocess_item(self, audio, sr):
-        if self.transform:
-            audio = AudioUtils.pad_trunc(audio, sr, 5000)
-        spec = AudioUtils.get_mel_spect(audio, sr, 128)
-        # spec_img = AudioUtils.spec_to_image(spec)
-        spec_img = spec
-        if self.model_name == "attVgg16":
-            spec_img = cv2.resize(spec_img, (224, 224))
+    def _filter_missing_audios(self):
+        filtered_audios = []
+        for row in self.audios:
+            _, audio_filename, _, system_id, key = row
+            file_full_path = os.path.join(self.base_path, self.partitions_path[self.partition],
+                                          audio_filename + ".wav" if self.is_asv19_silence_version else ".flac")
+            if os.path.exists(file_full_path):
+                filtered_audios.append(row)
+        np.savetxt("LA_CM_FILTERED.txt", np.array(filtered_audios), delimiter=" ", fmt='%s')
+        self.audios = np.array(filtered_audios)
 
-        if self.model_name == "passt2":
-            return audio, spec
-        else:
-            return spec_img[np.newaxis, ...], spec
+    def preprocess_item(self, audio, sr: int):
+        return preprocess_item(audio, sr, model_name=self.model_name,
+                               should_return_waveform=self.should_return_waveform, transform=self.transform,
+                               partition=self.partition, extract_manual_spec=self.extract_manual_spec)
+
+
+if __name__ == "__main__":
+    from dotenv import load_dotenv
+
+    load_dotenv()
+
+    # root dir should be .../ASV_ROOT/LA
+    dt = LoadAsvSpoof19(base_path=os.getenv("ASV-19-ROOT-DIR"),
+                        partition="training",
+                        transform=True, model_name="passt", mode="reduced")
+
+    dt = DataLoader(dt, batch_size=32, shuffle=False, num_workers=0,
+                    pin_memory=False)
+
+    iterator = iter(dt)
+    for batch in dt:
+        batch = next(iterator)
+
+        labels = batch[CLASS_KEY]
+        print("Labels: ", labels)
--- dataset/load_timi_dataset.py
+++ dataset/load_timi_dataset.py
@@ -1,14 +1,13 @@
 import os
 
-import cv2
 import librosa
 import numpy as np
 import pandas as pd
 from torch.utils.data import Dataset
 from typing_extensions import Literal
 
-from utils.audio_utils import AudioUtils
-from utils.timi_tts_constants import AUDIO_KEY, CLASS_KEY
+from dataset.loader_utils import preprocess_item
+from utils.timi_tts_constants import AUDIO_KEY, CLASS_KEY, ORIGINAL_SPEC_KEY
 
 
 class LoadTimiDataset(Dataset):
@@ -16,12 +15,18 @@ class LoadTimiDataset(Dataset):
     def __init__(self, base_path: str, metadata_file_path: str, partition: Literal["training", "validation", "test"],
                  model_name: Literal["resnet18", "resnet34", "resnet50", "attVgg16"],
                  is_validation_enabled: bool = True,
-                 transform: bool = False):
+                 transform: bool = False,
+                 should_return_waveform: bool = False,
+                 extract_manual_spec: bool = False):
 
         self.path = os.path.join(base_path, metadata_file_path)
         self.model_name = model_name
-        data = pd.read_csv(self.path)
+        self.extract_manual_spec = extract_manual_spec
+        self.should_return_waveform = should_return_waveform
+        self.partition = partition
+
         np.random.seed(123)
+        data = pd.read_csv(self.path)
         data = data.sample(frac=1, random_state=42)
 
         if is_validation_enabled:
@@ -29,23 +34,18 @@ class LoadTimiDataset(Dataset):
             # split in 60%, 20%, 20%
             left, middle, right = np.split(data, split_indices.astype(int))
             if partition == "training":
-                # take 60% dataframe
                 self.audios_df = left
             elif partition == "validation":
-                # take middle 20% of the dataframe
                 self.audios_df = middle
             elif partition == "test":
-                # take end 20% of the  dataframe
                 self.audios_df = right
         else:
             percent_66 = int(0.66 * len(data))
             # split in 66%, 33%
             left, right = np.split(data, [percent_66])
             if partition == "training":
-                # take 66% dataframe
                 self.audios_df = left
             elif partition == "test":
-                # take end 33% of the  dataframe
                 self.audios_df = right
 
         self.transform = transform
@@ -53,34 +53,14 @@ class LoadTimiDataset(Dataset):
     def __getitem__(self, index):
         audio_path, audio_class = self.audios_df.iloc[index]
         [audio, sr] = librosa.load(audio_path, sr=None)
-        if self.transform:
-            audio = AudioUtils.pad_trunc(audio, sr, 5000)
-        spec = AudioUtils.get_mel_spect(audio, sr, 128)
-        spec_img = AudioUtils.spec_to_image(spec)
-        if self.model_name == "attVgg16":
-            spec_img = cv2.resize(spec_img, (224, 224))
-        return {AUDIO_KEY: spec_img[np.newaxis, ...], CLASS_KEY: audio_class, "original": spec}
+        spec_img, spec = self.preprocess_item(audio, sr)
+
+        return {AUDIO_KEY: spec_img, CLASS_KEY: audio_class, ORIGINAL_SPEC_KEY: spec}
+
+    def preprocess_item(self, audio, sr: int):
+        return preprocess_item(audio, sr, model_name=self.model_name,
+                               should_return_waveform=self.should_return_waveform, transform=self.transform,
+                               partition=self.partition, extract_manual_spec=self.extract_manual_spec)
 
     def __len__(self):
         return self.audios_df.index.size
-
-
-if __name__ == "__main__":
-    data = pd.read_csv(r"C:\Users\IgnazioGulino\Desktop\my\IG\Thesis MASTER DEGREE\Datasets\TIMIT-TTS\clean.csv")
-    # x0, y0 = x[0]
-    p, p1 = data.iloc[0]
-
-    LoadTimiDataset(base_path=r"C:/Users/IgnazioGulino/Desktop/my/IG/Thesis MASTER DEGREE/Datasets/TIMIT-TTS",
-                    metadata_file_path="clean.csv", partition="training",
-                    transform=True, mode="reduced")
-    # split_indices = np.array([0.6, 0.8]) * len(data)
-    # # split in 60%, 20%, 20%
-    # left, middle, right = np.split(data, split_indices.astype(int))
-    # out = left[(left[CLASS_KEY] == 3) | (left[CLASS_KEY] == 4)]
-    # print(out, out.shape)
-    # len_data = len(x)
-    # split_indices = np.array([0.6, 0.8]) * len_data
-    # left, middle, right = np.split(x, split_indices.astype(int))
-    # take 60% dataframe
-
-    # print(left.shape, middle.shape, right.shape, middle)
